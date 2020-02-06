caf48a32cfff4823da52c9802884302c96c1ace3
Kish Jogia <kishjogia@yahoo.co.uk>
2020-02-06 15:48:08 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project Initialisation
+
+This will create a new directory, with an initialised git repository and synced to github
+
+## Note
+
+Add your own Personal Access Token
+Your SSH key will need to be added to your GitHub account
---
+++ project_init.py
@@ -0,0 +1,40 @@
+# ToDo
+# [x] 1. Create a new folder in home directory
+# [x] 2. Initalise git repository
+# [x] 3. Create a new repo in GitHub and link
+
+import sys
+import os
+from github import Github
+
+home_path = "/home/kishj/Dropbox/development/"              # Path to store development work
+access_token = ""   # Personal access token from GitHub
+git_user = "kishjogia"
+
+def project_init():
+    if len(sys.argv) == 2:
+        folderName = str(sys.argv[1])
+        os.makedirs(home_path + folderName)             # Create a new directory
+
+        # Create a git repository
+        os.chdir(home_path + folderName)
+        os.system('git init')
+
+        # Create a readme file and do an Inital commit
+        os.system('echo "# ' + folderName + '" >> README.md')
+        os.system('git add .')
+        os.system('git commit -m "Initial commit"')
+
+        g_hub = Github(access_token).get_user()         # Login using Personal Access Token
+        g_hub.create_repo(folderName)                   # Create repository in Github
+
+        os.system('git remote add origin git@github.com:' + git_user +'/' + folderName + '.git')
+        os.system('git push -u origin master')
+    else:
+        print("Need a name for the Project...Exiting")
+        return
+
+    return
+
+if __name__ == "__main__":
+    project_init()
