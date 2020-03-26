035617f081e533bd09b125ddac84709da0461614
Kish Jogia <kishjogia@yahoo.co.uk>
2020-03-26 08:48:28 +0000

    Update to pat - Moving to use OneDrive

--- project_init.py
+++ project_init.py
@@ -7,8 +7,8 @@ import sys
 import os
 from github import Github
 
-home_path = "/home/kishj/Dropbox/development/"              # Path to store development work
-access_token = ""   # Personal access token from GitHub
+home_path = "/home/kishj/OneDrive/development"              # Path to store development work
+access_token = "c98ac6c9b89bcf66dae21726efc8514d251304e7"   # Personal access token from GitHub
 git_user = "kishjogia"
 
 def project_init():
